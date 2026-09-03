6fcadff6d72a4afa6a5777dab2d3d7d384173cab
Sarthak752008 <sarthak752008@gmail.com>
2026-09-04 03:47:55 +0530

    chore(ops): remove individual startup scripts, keeping only unified start/stop scripts

--- start_frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting AeroSense Frontend...
-cd /d "%~dp0frontend"
-if not exist "node_modules" (
-    echo Installing dependencies...
-    npm install
-)
-npm run dev
--- start_frontend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting AeroSense Frontend..."
-cd "$(dirname "$0")/frontend"
-if [ ! -d "node_modules" ]; then
-    echo "Installing dependencies..."
-    npm install
-fi
-npm run dev
